4c11b2a92c20c67eedcaaf21b8d7fd7d77ee52de
PedroDev12 <tu_correo@ejemplo.com>
2025-08-22 15:05:35 +0200

    first commit

---
+++ src/app.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  getHello(): string {
+    return 'Hello NestJS';
+    
+  }
+
+  postHello() {
+    return "Desde @Post en el service"
+  }
+  
+}
